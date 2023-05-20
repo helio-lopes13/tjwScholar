60d194cb82a0d77be2b79baec2e1f7e3a9c44fc1
Hélio Lopes <heliolopesalves13@gmail.com>
2023-05-20 14:03:49 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/edu/ifce/tjwScholar/TjwScholarApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifce.tjwScholar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TjwScholarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TjwScholarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifce/tjwScholar/TjwScholarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifce.tjwScholar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TjwScholarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
